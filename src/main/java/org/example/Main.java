380aa0a7a0f2007274634c755c1cafd6ec2093df
Yullc <apanan0910@gmail.com>
2025-04-04 16:15:24 +0900

    test

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] arr = new int[]{1,2,3,4};
+
+        s.solution(arr);
+    }
+    static class Solution {
+        public double solution(int[] arr) {
+            double answer = 0;
+
+            return answer;
+        }
+    }
+}
