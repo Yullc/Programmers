62fd6c9b06901945aecc370aae8308874a457982
Yullc <apanan0910@gmail.com>
2025-04-08 15:14:10 +0900

    중복된 숫자 개수

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,16 +2,20 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        Solution s = new Solution();
-        int[] arr = new int[]{1,2,3,4};
-
-        s.solution(arr);
+    Solution s = new Solution();
+    int[] array = new int[]{1, 1, 2, 3, 4, 5};
+    s.solution(array, 1);
     }
-    static class Solution {
-        public double solution(int[] arr) {
-            double answer = 0;
-
-            return answer;
+    }
+class Solution {
+    public int solution(int[] array, int n) {
+        int answer = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == n) {
+                answer++;
+            }
         }
+        System.out.println(answer);
+        return answer;
     }
 }
