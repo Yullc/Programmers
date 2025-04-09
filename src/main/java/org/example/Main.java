623a617cbe061e2483d4204a7a55c53bc05baecb
Yullc <apanan0910@gmail.com>
2025-04-09 18:40:11 +0900

    음양 더하기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,23 +1,56 @@
 package org.example;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-    Solution s = new Solution();
-   int[] numbers = new int[]{1, 2, 3, 4, 5};
-    s.solution(numbers);
+        Solution sol = new Solution();
+        int[] absolutes = new int[]{4,7,12};
+        boolean[] signs = new boolean[absolutes.length];
+        String s = "addp";
+        sol.solution(absolutes,signs);
     }
-    }
-class Solution {
-    public List<Integer> solution(int[] numbers) {
-        List<Integer> answer = new ArrayList<>();
-        for (int i = 0; i < numbers.length; i++) {
-            numbers[i] = numbers[i] + numbers[i];
-            answer.add(numbers[i]);
-        }
+}
 
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        for (int i = 0; i < absolutes.length; i++) {
+            if (signs[i]) {
+                answer += absolutes[i];
+            } else {
+                answer -= absolutes[i];
+            }
+        }
+        System.out.println(answer);
         return answer;
     }
 }
+
+//class Solution {
+//    boolean solution(String s) {
+//        boolean answer = true;
+//        int count=0;
+//        int num =0;
+//        String[] st = s.split("");
+//        for(int i = 0; i < st.length; i++) {
+//            if(st[i].equals("p")) {
+//                count++;
+//            }
+//            else if(st[i].equals("y")) {
+//                num++;
+//            }
+//
+//        }
+//        if(count != num) {
+//            answer = false;
+//        }
+//        else if (count == 0 && num == 0) {
+//            answer = true;
+//        }
+//        System.out.println(answer);
+//        return answer;
+//    }
+//}
