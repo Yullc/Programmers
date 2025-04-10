cb069217f38b660fc0e75c4ffb1ce75f12004029
Yullc <apanan0910@gmail.com>
2025-04-10 14:01:53 +0900

    삼총사

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,30 +7,31 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         Solution sol = new Solution();
-        int[] absolutes = new int[]{4,7,12};
-        boolean[] signs = new boolean[absolutes.length];
+        int[] number = new int[]{-2, 3, 0, 2, -5};
         String s = "addp";
-        sol.solution(3,30,5);
+        sol.solution(number);
     }
 }
 
 class Solution {
-    public long solution(int price, int money, int count) {
-        long answer=0;
-        long total=0 ;
-        for(int i=1;i<=count;i++){
-            total += price *i;
+    public int solution(int[] number) {
+        int answer = 0;
+        int count = 0;
+        for (int i = 0; i < number.length; i++) {
+            for (int j = i + 1; j < number.length; j++) {
+                for (int k = j + 1; k < number.length; k++) {
+                    if (number[i] + number[j] + number[k] == 0) {
+                        answer++;
+                    }
+                }
+            }
+
+            }
+        return answer;
         }
-        if (total > money) {
-            answer = total-money;
-            return answer;
-        } else if (total == money) {
-            return 0;
-        } else{
-            return 0;
-        }
+
     }
-}
+
 
 //class Solution {
 //    boolean solution(String s) {
