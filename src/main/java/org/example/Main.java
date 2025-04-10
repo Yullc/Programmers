340cab86035706c96102c1465e9164ef3d498084
Yullc <apanan0910@gmail.com>
2025-04-10 21:57:05 +0900

    부분자리 배열 최종

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,60 +1,40 @@
 package org.example;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         Solution sol = new Solution();
-        int[] number = new int[]{-2, 3, 0, 2, -5};
-        String s = "addp";
-        sol.solution(number);
+
+        String t = "3141592";
+        String p = "271";
+
+        int result = sol.solution(t, p);
+        System.out.println("결과: " + result);
     }
 }
 
 class Solution {
-    public int solution(int[] number) {
+    public int solution(String t, String p) {
         int answer = 0;
-        int count = 0;
-        for (int i = 0; i < number.length; i++) {
-            for (int j = i + 1; j < number.length; j++) {
-                for (int k = j + 1; k < number.length; k++) {
-                    if (number[i] + number[j] + number[k] == 0) {
-                        answer++;
-                    }
-                }
-            }
 
+        if (p.length() > t.length()) return 0;
+
+        long pLong = Long.parseLong(p);
+
+        for (int i = 0; i <= t.length() - p.length(); i++) {
+            String sub = t.substring(i, i + p.length());
+
+            try {
+                long subLong = Long.parseLong(sub);
+                if (subLong <= pLong) {
+                    answer++;
+                }
+            } catch (NumberFormatException e) {
             }
-        return answer;
         }
 
+        return answer;
     }
-
-
-//class Solution {
-//    boolean solution(String s) {
-//        boolean answer = true;
-//        int count=0;
-//        int num =0;
-//        String[] st = s.split("");
-//        for(int i = 0; i < st.length; i++) {
-//            if(st[i].equals("p")) {
-//                count++;
-//            }
-//            else if(st[i].equals("y")) {
-//                num++;
-//            }
-//
-//        }
-//        if(count != num) {
-//            answer = false;
-//        }
-//        else if (count == 0 && num == 0) {
-//            answer = true;
-//        }
-//        System.out.println(answer);
-//        return answer;
-//    }
-//}
+}
